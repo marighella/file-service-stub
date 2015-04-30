60bd328e9a3cc1e397cb4b22acd9ec180f324e7f
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-04-30 10:30:24 -0300

    Adicionando novo atributo basename para retorno do titulo sem extensão

--- lib/google_drive.rb
+++ lib/google_drive.rb
@@ -7,7 +7,8 @@ module Service
       {
         link: 'https://docs.google.com/uc?id=0B46YLSm1qo-6eE1WeHdQalF3S0k&export=download',
         thumbnail: 'http://farm4.staticflickr.com/3939/15458051067_f2f7afa6e8_b.jpg',
-        title: 'Racionais Mc\'s - Mil Faces de um Homem Leal (Carlos Marighella) - Videoclipe Oficial.mp3'
+        title: 'Racionais Mc\'s - Mil Faces de um Homem Leal (Carlos Marighella) - Videoclipe Oficial.mp3',
+        basename: 'Racionais Mc\'s - Mil Faces de um Homem Leal (Carlos Marighella) - Videoclipe Oficial',
       }
     end
   end
