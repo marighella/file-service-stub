1dcfe069b86cedf7f09dd2af10b51cf35bd3e37d
Rodrigo <rodrigovieira18@gmail.com>
2015-11-26 10:19:29 -0200

    Removed default protocol

--- lib/flickr.rb
+++ lib/flickr.rb
@@ -1,10 +1,10 @@
 module Service
   class Flickr
     def upload(image_path, file_name)
-      links = { link:'http://farm9.staticflickr.com/8796/17306389125_7f60267c76_b.jpg',
-                thumbnail:'http://farm9.staticflickr.com/8796/17306389125_7f60267c76_t.jpg',
-                medium:'http://farm9.staticflickr.com/8796/17306389125_7f60267c76_z.jpg',
-                small:'http://farm9.staticflickr.com/8796/17306389125_7f60267c76_n.jpg',
+      links = { link:'//farm9.staticflickr.com/8796/17306389125_7f60267c76_b.jpg',
+                thumbnail:'//farm9.staticflickr.com/8796/17306389125_7f60267c76_t.jpg',
+                medium:'//farm9.staticflickr.com/8796/17306389125_7f60267c76_z.jpg',
+                small:'//farm9.staticflickr.com/8796/17306389125_7f60267c76_n.jpg',
                 title: 'Carlos_Marighella.jpg'}
 
       links.inject({}) do |hash, (key,value)|
