7b5aa30b42c22ca53ee2d6d0bff81f4f522133fc
Rodrigo Alencar <rodrigovieira18@gmail.com>
2017-06-20 22:17:55 -0300

    Put https on protocol

--- lib/flickr.rb
+++ lib/flickr.rb
@@ -1,14 +1,13 @@
 module Service
   class Flickr
     def upload(image_path, file_name)
-      links = { link:'//farm9.staticflickr.com/8796/17306389125_7f60267c76_b.jpg',
-                thumbnail:'//farm9.staticflickr.com/8796/17306389125_7f60267c76_t.jpg',
-                medium:'//farm9.staticflickr.com/8796/17306389125_7f60267c76_z.jpg',
-                small:'//farm9.staticflickr.com/8796/17306389125_7f60267c76_n.jpg',
+      links = { link:'https://farm9.staticflickr.com/8796/17306389125_7f60267c76_b.jpg',
+                thumbnail:'https://farm9.staticflickr.com/8796/17306389125_7f60267c76_t.jpg',
+                medium:'https://farm9.staticflickr.com/8796/17306389125_7f60267c76_z.jpg',
+                small:'https://farm9.staticflickr.com/8796/17306389125_7f60267c76_n.jpg',
                 title: 'Carlos_Marighella.jpg'}
 
       links.inject({}) do |hash, (key,value)|
-        hash[key] = value.gsub('https://','http://')
         hash
       end
     end
